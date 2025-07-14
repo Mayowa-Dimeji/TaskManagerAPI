4fcea192cf9b092d9da22e92dabeca824ee71bc8
Mayowa Oladimeji <dimejikassie@gmail.com>
2025-07-14 12:33:50 +0100

    register and login routes with jwt auth

--- obj/Debug/net9.0/TaskManagerApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/TaskManagerApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31f891b8533ef8bfe2127f9274b12fd2998b8e90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f4ba332caa58482532fae08bf658e148f1580f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
